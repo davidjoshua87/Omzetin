327394df17e92033b245bd2001eed976b4874bc8
David Joshua <davidjosh_87@yahoo.com>
2018-04-25 13:32:12 +0700

    added file index.ejs

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const indexRoutes = require('./routes');
+const providersRoutes = require('./routes/provider');
+
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Body Parser
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/', indexRoutes);
+app.use('/', providersRoutes);
+
+// -----------------------------------------------------------------------------
+app.listen(port, console.log(`Listening on port ${port}`));
